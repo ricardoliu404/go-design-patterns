306fdd4a8c856a7f79a236c65917cd6c3d73ffd7
ricardoliu404 <herolzx707@gmail.com>
2020-02-24 12:04:55 +0800

    singleton and builder

---
+++ creational/builder/ManufacturingDirector.go
@@ -0,0 +1,13 @@
+package builder
+
+type ManufacturingDirector struct {
+	builder BuildProcess
+}
+
+func (f *ManufacturingDirector) Construct() {
+	f.builder.SetSeats().SetStructure().SetWheels()
+}
+
+func (f *ManufacturingDirector) SetBuilder(b BuildProcess) {
+	f.builder = b
+}
