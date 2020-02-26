204cd239e618679d081a11513f3226cab66e85bf
ricardoliu404 <herolzx707@gmail.com>
2020-02-26 14:34:46 +0800

    finish factory method

---
+++ creational/factory/CashPM.go
@@ -0,0 +1,14 @@
+package factory
+
+import "fmt"
+
+type CashPM struct{}
+type DebitCardPM struct{}
+
+func (c *CashPM) Pay(amount float32) string {
+	return fmt.Sprintf("%0.2f paid using cash\n", amount)
+}
+
+func (d *DebitCardPM) Pay(amount float32) string {
+	return fmt.Sprintf("%0.2f paid using debit card\n", amount)
+}
