e50347d4ad5118d3af1dca60af6ca0ffc3a11740
ricardoliu404 <herolzx707@gmail.com>
2020-03-04 14:29:02 +0800

    bridge pattern

---
+++ structural/bridge/printerabstraction.go
@@ -0,0 +1,29 @@
+package bridge
+
+import (
+	"fmt"
+)
+
+type PrinterAbstraction interface {
+	Print() error
+}
+
+type NormalPrinter struct {
+	Msg     string
+	Printer PrinterAPI
+}
+
+func (c *NormalPrinter) Print() error {
+	c.Printer.PrintMessage(c.Msg)
+	return nil
+}
+
+type PacktPrinter struct {
+	Msg     string
+	Printer PrinterAPI
+}
+
+func (c *PacktPrinter) Print() error {
+	c.Printer.PrintMessage(fmt.Sprintf("Message from Packt: %s", c.Msg))
+	return nil
+}
---
+++ structural/bridge/printerapi.go
@@ -0,0 +1,46 @@
+package bridge
+
+import (
+	"errors"
+	"fmt"
+	"io"
+)
+
+type PrinterAPI interface {
+	PrintMessage(string) error
+}
+
+type PrinterImpl1 struct{}
+
+func (p *PrinterImpl1) PrintMessage(msg string) error {
+	fmt.Printf("%s\n", msg)
+	return nil
+	//return errors.New("Not implemented yet")
+}
+
+type PrinterImpl2 struct {
+	Writer io.Writer
+}
+
+func (p *PrinterImpl2) PrintMessage(msg string) error {
+	if p.Writer == nil {
+		return errors.New("You need to pass an io.Writer to PrinterImpl2")
+	}
+	_, _ = fmt.Fprintf(p.Writer, "%s", msg)
+	return nil
+	//return errors.New("Not implemented yet")
+}
+
+type TestWriter struct {
+	Msg string
+}
+
+func (t *TestWriter) Write(p []byte) (n int, err error) {
+	n = len(p)
+	if n > 0 {
+		t.Msg = string(p)
+		return n, nil
+	}
+	err = errors.New("Content received on Writer was empty")
+	return
+}
